4a5f5507800e008863d7a9ab3efc320431833358
tzuk polinsky <tzuk9800@gmail.com>
2025-07-02 15:13:46 +0300

    removed bukah

--- tabs_report/tabs_report_generator.py
+++ tabs_report/tabs_report_generator.py
@@ -188,11 +188,6 @@ def _generate_nested_html_template(html_files, report_title, current_datetime):
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-        <link rel="stylesheet" href="https://cdn.bokeh.org/bokeh/release/bokeh-3.3.0.min.css" type="text/css" />
-
-        <script src="https://cdn.bokeh.org/bokeh/release/bokeh-3.3.0.min.js"></script>
-        <script src="https://cdn.bokeh.org/bokeh/release/bokeh-widgets-3.3.0.min.js"></script>
-        <script src="https://cdn.bokeh.org/bokeh/release/bokeh-tables-3.3.0.min.js"></script>
         <title>{report_title}</title>
         {custom_css}
     </head>
@@ -267,11 +262,6 @@ def _generate_single_html_template(html_file, report_title, current_datetime):
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-        <link rel="stylesheet" href="https://cdn.bokeh.org/bokeh/release/bokeh-3.3.0.min.css" type="text/css" />
-
-        <script src="https://cdn.bokeh.org/bokeh/release/bokeh-3.3.0.min.js"></script>
-        <script src="https://cdn.bokeh.org/bokeh/release/bokeh-widgets-3.3.0.min.js"></script>
-        <script src="https://cdn.bokeh.org/bokeh/release/bokeh-tables-3.3.0.min.js"></script>
         <title>{report_title}</title>
         {custom_css}
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
@@ -370,11 +360,6 @@ def _generate_flat_html_template(html_files, report_title, current_datetime):
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-        <link rel="stylesheet" href="https://cdn.bokeh.org/bokeh/release/bokeh-3.3.0.min.css" type="text/css" />
-
-        <script src="https://cdn.bokeh.org/bokeh/release/bokeh-3.3.0.min.js"></script>
-        <script src="https://cdn.bokeh.org/bokeh/release/bokeh-widgets-3.3.0.min.js"></script>
-        <script src="https://cdn.bokeh.org/bokeh/release/bokeh-tables-3.3.0.min.js"></script>
         <title>{report_title}</title>
         {custom_css}
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
